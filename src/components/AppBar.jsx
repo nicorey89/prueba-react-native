7c554807771fa9f88a07d439a3d08dde834ce048
nicorey89 <nicolasthian8907@gmail.com>
2024-01-28 17:05:39 -0300

    arreglando problemas de rutas

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,35 +1,51 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import StyledText from "./StyledText.jsx";
 import Constants from "expo-constants";
-import { Link } from "react-router-native";
+import { Link, useLocation } from "react-router-native";
 
 const styles = StyleSheet.create({
     appBar: {
         backgroundColor: "blue",
         flexDirection: "row",
         justifyContent: "space-around",
-        paddingTop: Constants.statusBarHeight + 10,
-        paddingBottom: 10,
-        paddingLeft: 10
+        paddingTop: Constants.statusBarHeight + 10
+    },
+    scroll: {
+        paddingBottom: 15
+    },
+    text: {
+        color: "grey",
+        paddingHorizontal: 10
+    },
+    active: {
+        color: "white"
     }
 })
 
 const AppBarTab = ({ children, to }) => {
+    const { pathname } = useLocation()
+    const active = pathname == to
+    const textActive = [
+        styles.text,
+        active && styles.active
+    ]
     return(
         <Link to={to}>
-            <StyledText bold white>{children}</StyledText>
+            <StyledText bold styles={textActive}>{children}</StyledText>
         </Link>
     )
 }
 
 const AppBar = () => {
     return (
-        <View style={styles.appBar}>
-            <AppBarTab active={true} to="/">Repositories</AppBarTab>
-            <AppBarTab active={true} to="/signin">Singin</AppBarTab>
+        <View  style={styles.appBar}>
+            <ScrollView horizontal style={styles.scroll}>
+                <AppBarTab to="/">Repositories</AppBarTab>
+                <AppBarTab to="/signin">Sing In</AppBarTab>
+            </ScrollView>
         </View> 
-    );
+    )
 }
 
-export default AppBar;
+export default AppBar
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { View, Text  } from 'react-native';
-import RepositoryList from "./RepositoryList.jsx";
-import AppBar from "./AppBar.jsx";
-import { Route } from "react-router-native";
+import React from "react"
+import { View, Text} from 'react-native'
+import RepositoryList from "./RepositoryList.jsx"
+import AppBar from "./AppBar.jsx"
+import { Navigate, Route, Routes} from "react-router-native"
 
 const Main = () => {
     return (
         <View style={{ flex: 1 }}>
             <AppBar/>
-            <RepositoryList />
+            <Routes>
+                <Route path='/' element={<RepositoryList />} />
+                <Route path='/signin' element={<Text>Sign In</Text>} />
+                <Route path='*' element={<Navigate to='/' />} />
+            </Routes>
         </View>
-    );
+    )
 }
 
-export default Main;
+export default Main
